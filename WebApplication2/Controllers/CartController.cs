5d3e6b8f550936674cd5dec5b5b59fcb2ee8133f
kipkup <kipkup@yahoo.com>
2021-01-16 09:48:28 +0200

    Change Delete route.

--- WebApplication2/Controllers/CartController.cs
+++ WebApplication2/Controllers/CartController.cs
@@ -24,7 +24,7 @@ namespace WebApplication2.Controllers
         [Route("~/[controller]/[action]/{id}")]
         public ListCartResponse List(int id)
         {
-            var res = _cartService.ListCart(new ListCartRequest() { UserId = id });
+            var res = _cartService.ListCart(new ListCartRequest { UserId = id });
             return res;
         }
 
@@ -36,9 +36,10 @@ namespace WebApplication2.Controllers
         }
 
         [HttpDelete]
-        public bool Delete([FromBody] DeleteCartRequest request)
+        [Route("~/[controller]/[action]/{id}")]
+        public bool Delete(int id)
         {
-            var res = _cartService.DeleteCart(request);
+            var res = _cartService.DeleteCart(new DeleteCartRequest { UserId = id });
             return res;
         }
 
